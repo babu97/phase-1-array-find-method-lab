3327e31bf07b70d7cf0367f9f5afbde11044a86d
babu97 <benkaimugul@gmail.com>
2023-09-29 09:04:36 +0300

    finished assignments

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+  ];
+  
+  const yearOfWin = superbowlWin(record);
+  if (yearOfWin) {
+    console.log(`The Denver Broncos won the Super Bowl in ${yearOfWin}`);
+  } else {
+    console.log("The Denver Broncos have not won a Super Bowl.");
+  }
+
+  function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
